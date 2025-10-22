843a19e77ff4e2f8e1c0932dd044762b8c710929
jpqin <3513461372@qq.com>
2025-10-22 11:19:49 +0800

    add model

---
+++ models/envnet.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+from modules import *
+
+__all__ = ["create_model", "EnvNet3D"]
+
+def create_model(cfg='config.yaml'):
+    import yaml, os
+    if isinstance(cfg, str) and os.path.exists(cfg):
+        with open(cfg, 'r') as f:
+            cfg = yaml.safe_load(f)
+    return EnvNet3D(hidden_width=cfg['hidden_width'], padding_mode=cfg['padding_mode'], factorization=cfg['factorization'])
+
+
+class EnvNet3D(nn.Module):
+    def __init__(self, hidden_width=5, padding_mode='circular', factorization=False):
+        super().__init__()
+        # Backbone
+        self.multiconv1 = MultiConv3d(2, hidden_width, 3, 7, 9, padding_mode, True, factorization=factorization)
+        self.conv1 = ConvBN3d(hidden_width, hidden_width*2, 3, 2, padding_mode, factorization=factorization)
+        self.multiconv2 = MultiConv3d(hidden_width*2, hidden_width*2, 3, 5, 7, padding_mode, True, factorization=factorization)
+        self.conv2 = ConvBN3d(hidden_width*2, hidden_width*4, 3, 2, padding_mode, factorization=factorization)
+        self.multiconv3 = MultiConv3d(hidden_width*4, hidden_width*4, 3, 3, 5, padding_mode, True, factorization=factorization)
+        self.conv3 = ConvBN3d(hidden_width*4, hidden_width*2, 3, 1, padding_mode, factorization=factorization)
+
+        # Neck
+        self.upcatconv1 = UpCatConv3d(hidden_width*2, hidden_width, 3, 1, padding_mode, factorization=factorization)
+        self.upcatconv2 = UpCatConv3d(hidden_width, hidden_width, 3, 1, padding_mode, factorization=factorization)
+        self.downcatconv1 = DownCatConv3d(hidden_width, hidden_width*2, 3, 1, padding_mode, factorization=factorization)
+        self.downcatconv2 = DownCatConv3d(hidden_width*2, hidden_width*4, 3, 1, padding_mode, factorization=factorization)
+
+        # Head
+        self.head1 = HeadConv3d(hidden_width, 3, 3, 1, padding_mode, factorization=factorization)
+        self.head2 = HeadConv3d(hidden_width*2, 3, 3, 1, padding_mode, factorization=factorization)
+        self.head3 = HeadConv3d(hidden_width*4, 3, 3, 1, padding_mode, factorization=factorization)
+
+    def forward(self, x):
+        # x: (B, C, D, H, W)
+        x1 = self.multiconv1(x)
+        x2 = self.multiconv2(self.conv1(x1))
+        x3 = self.multiconv3(self.conv2(x2))
+
+        x3_2 = self.conv3(x3)
+        x2_2 = self.upcatconv1(x3_2, x2)
+        x1_2 = self.upcatconv2(x2_2, x1)
+
+        x1_3 = x1_2
+        x2_3 = self.downcatconv1(x1_3, x2_2)
+        x3_3 = self.downcatconv2(x2_3, x3_2)
+
+        out1 = self.head1(x1_3)
+        out2 = self.head2(x2_3)
+        out3 = self.head3(x3_3)
+
+        return [out1, out2, out3]
+
+if __name__ == "__main__":
+    model = EnvNet3D(hidden_width=8)
+    print(model)
+    x = torch.randn(2, 2, 8, 32, 32)  # B, C, D, H, W
+    y = model(x)
+    print([o.shape for o in y])
---
+++ models/modules.py
@@ -0,0 +1,94 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from collections import OrderedDict
+
+
+class Conv3dPad(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding_mode="zeros", groups=1, bias=False):
+        super().__init__()
+        assert padding_mode in ['zeros', 'circular']
+        self.padding_mode = padding_mode
+        if isinstance(kernel_size, int):
+            self.padding = [(kernel_size - 1) // 2] * 3
+        else:
+            self.padding = [(k - 1) // 2 for k in kernel_size]
+        self.conv = nn.Conv3d(in_channels, out_channels, kernel_size, stride, padding=0, groups=groups, bias=bias)
+
+    def forward(self, x):
+        # 3D padding: (W, H, D)
+        if self.padding_mode == 'circular':
+            x = F.pad(x, (self.padding[2], self.padding[2], self.padding[1], self.padding[1],
+                          self.padding[0], self.padding[0]), mode='circular')
+        else:
+            x = F.pad(x, (self.padding[2], self.padding[2], self.padding[1], self.padding[1],
+                          self.padding[0], self.padding[0]), mode='constant', value=0)
+        return self.conv(x)
+
+
+class ConvBN3d(nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding_mode='zeros', groups=1, bias=False, factorization=False):
+        modules = []
+        if not factorization:
+            modules.append(("conv", Conv3dPad(in_channels, out_channels, kernel_size, stride, padding_mode, groups, bias)))
+        else:
+            # 分解卷积：沿不同维度逐步卷积
+            modules.append(("conv_d", Conv3dPad(in_channels, out_channels, (kernel_size, 1, 1), 1, padding_mode, groups, bias)))
+            modules.append(("conv_hw", Conv3dPad(out_channels, out_channels, (1, kernel_size, kernel_size), stride, padding_mode, groups, bias)))
+
+        modules.append(("bn", nn.BatchNorm3d(out_channels)))
+        modules.append(("act", nn.SiLU()))
+        super().__init__(OrderedDict(modules))
+
+
+class MultiConv3d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_in, kernel_1, kernel_2, padding_mode='zeros', shortcut=True, groups=1, factorization=False):
+        super().__init__()
+        self.in_conv = ConvBN3d(in_channels, out_channels, kernel_in, 1, padding_mode, groups, factorization)
+        self.conv1 = ConvBN3d(out_channels, out_channels, kernel_1, 1, padding_mode, groups, factorization)
+        self.conv2 = ConvBN3d(out_channels, out_channels, kernel_2, 1, padding_mode, groups, factorization)
+        self.add = shortcut
+        self.conv1d = ConvBN3d(2 * out_channels, out_channels, 1, 1, padding_mode="zeros", groups=groups)
+        self.act = nn.SiLU()
+
+    def forward(self, x):
+        out = self.in_conv(x)
+        cat = torch.cat((self.conv1(out), self.conv2(out)), dim=1)
+        out = self.conv1d(cat)
+        return self.act(out + self.in_conv(x)) if self.add else self.act(out)
+
+
+class UpCatConv3d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding_mode='zeros', groups=1, factorization=False):
+        super().__init__()
+        self.up = nn.Upsample(scale_factor=2, mode='trilinear', align_corners=False)
+        self.conv = nn.Sequential(OrderedDict([
+            ("conv1", ConvBN3d(in_channels * 2, in_channels, kernel_size, stride, padding_mode, groups, factorization)),
+            ("conv2", ConvBN3d(in_channels, out_channels, kernel_size, stride, padding_mode, groups, factorization))
+        ]))
+
+    def forward(self, x1, x2):
+        return self.conv(torch.cat((self.up(x1), x2), dim=1))
+
+
+class DownCatConv3d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding_mode='zeros', groups=1, factorization=False):
+        super().__init__()
+        self.downconv = ConvBN3d(in_channels, in_channels, kernel_size, stride=2, padding_mode=padding_mode, groups=groups, factorization=factorization)
+        self.conv = ConvBN3d(in_channels * 2, out_channels, kernel_size, stride, padding_mode=padding_mode, groups=groups, factorization=factorization)
+
+    def forward(self, x1, x2):
+        return self.conv(torch.cat((self.downconv(x1), x2), dim=1))
+
+
+class HeadConv3d(nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding_mode='zeros', groups=1, factorization=False):
+        layer = []
+        if not factorization:
+            layer.append(("conv", Conv3dPad(in_channels, out_channels, kernel_size, stride, padding_mode, groups, bias=False)))
+        else:
+            layer.append(("conv1", Conv3dPad(in_channels, out_channels, (kernel_size, 1, 1), stride, padding_mode, groups, bias=False)))
+            layer.append(("conv2", Conv3dPad(out_channels, out_channels, (1, kernel_size, kernel_size), stride, padding_mode, groups, bias=False)))
+        layer.append(("bn", nn.BatchNorm3d(out_channels)))
+        layer.append(("act", nn.LeakyReLU(negative_slope=0.3, inplace=True)))
+        super().__init__(OrderedDict(layer))
